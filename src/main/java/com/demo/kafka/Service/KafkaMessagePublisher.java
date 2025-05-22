f8d4676ad09c39436984c0bd5ac0ea893cbf6896
SaiPrashanthDSP <saiprashanthdyapa.uf1998@gmail.com>
2025-05-22 16:55:52 -0500

    creating a sample producer for kafka

---
+++ src/main/java/com/demo/kafka/Service/KafkaMessagePublisher.java
@@ -0,0 +1,25 @@
+package com.demo.kafka.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+@Service
+public class KafkaMessagePublisher {
+    @Autowired
+    private KafkaTemplate<String,Object> template;
+
+    public void sendMessageToTopic(String message) {
+        CompletableFuture<SendResult<String, Object>> future =  template.send("sample-topic",message);
+        future.whenComplete((result,ex) -> {
+                if(ex == null) {
+                    System.out.println(message + "With Offset ="+ result.getRecordMetadata().offset());
+                    }
+                else {
+                    System.out.println("Unable to send message due to "+ex.getMessage());
+                }
+        });
+    }
+}
